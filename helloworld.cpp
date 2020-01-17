f0a11864096fed294101b68f62f0595a6049b796
Sujit Gurung <gurungs4@vcu.edu>
2020-01-16 21:27:24 -0500

    HelloWorld

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+/* Program #1 - A first C++ program.
+Enter this program, then compile and run it. 
+*/
+
+#include <iostream>
+using namespace std;
+
+// main() is where program execution begins. 
+int main()
+{
+    cout << "This is my first program!" << endl;
+    return 0;
+}
